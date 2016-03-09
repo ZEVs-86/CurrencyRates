378148b04b46e640b9cb3562f2c776e268400d21
Evgeny Zakharov <evgenyzakharov@evgenys-mac.local>
2016-03-09 21:00:54 +0300

    Initial Commit

---
+++ CurrencyRates/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CurrencyRates
+//
+//  Created by Evgeny Zakharov on 3/9/16.
+//  Copyright © 2016 Evgeny Zakharov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CurrencyRates/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CurrencyRates
+//
+//  Created by Evgeny Zakharov on 3/9/16.
+//  Copyright © 2016 Evgeny Zakharov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
